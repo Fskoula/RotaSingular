3ade68c69f37b901bd12e862e380427c3ca13fb8
unknown <jenniiifer.peters@gmail.com>
2025-07-08 20:15:44 -0300

    primeiro commit do projeto

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+
+export default function Home() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Perfil</li>
+          <li class="meio">Novo roteiro</li>
+          <li>Home</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
