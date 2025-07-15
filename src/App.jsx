df0e56e0a78f666ef6cf32e79f173933f0d1788b
Bárbara Rodrigues <barbararod19@outlook.com>
2025-07-15 18:57:01 -0300

    Tela de Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Home from './components/home/Home'
-
+import Login from './components/login/Login';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-      <Home/>
-      </div>
-      
-
-    </>
-  )
+    <div className='App'>
+      <Login/>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import { FaUser, FaLock } from 'react-icons/fa';
 import './Login.css'
 
-export default function Login() {
-  return (
-    <div>
-        Login
+const Login = () => {
+ return (
+  <div className='container'>
+   <form>
+    <h1>Login</h1>
+    <h3>Que bom te ver de novo, viajante!
+      <br/> Sentimos sua falta.</h3>
+    <div className= 'input-field'>
+    <input type='email' placeholder='E-mail'/>
+    <FaUser className='icon' />
+</div>
+<div className='input-field'>
+    <input type='password' placeholder='Senha'/>
+    <FaLock  className='icon' />
     </div>
-  )
+
+    <div className='recall-forget'>
+      <label>
+    <input type='checkbox'/>
+    Lembre de mim 
+      </label>
+      <a href='#'>Esqueceu a senha?</a>
+    </div>
+   
+   <button>Entrar</button>
+  <div className='signup-link'>
+    <p>Não possui registro? <a href='#'>Crie uma conta</a></p>
+  </div>
+   </form>
+  </div>
+);
 }
+
+export default Login;
